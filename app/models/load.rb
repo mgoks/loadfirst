6141fc864f5de405c8670563e45ce04c1a447f44
Murat Göksel <hmgoksel@gmail.com>
2023-03-25 19:33:05 -0400

    Validate equipment type

--- app/models/load.rb
+++ app/models/load.rb
@@ -1,8 +1,18 @@
 class Load < ApplicationRecord
-  # EQUIPMENT_TYPES = %w['dry van' 'flat deck' 'reefer'].freeze
 
-  # validates :equipment_type, presence: true, inclusion: {
-  #   in: EQUIPMENT_TYPES
-  # }
+  EQUIPMENT_TYPES = %w[dry_van reefer flat_deck].freeze
+
+  validates :delivery_location,
+            :pickup_date,
+            :shipper_email,
+            :shipper_phone,
+            :pickup_location,
+            presence: true
+
+  validates :equipment_type, presence: true, inclusion: { 
+    in: EQUIPMENT_TYPES, 
+    message: "%{value} is not a valid equipment type. 
+        Allowed types are #{EQUIPMENT_TYPES.join(', ')}." 
+  }
 
 end
