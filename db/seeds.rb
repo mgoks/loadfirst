fa943c7836f854bb2328375990ea7d57b839671f
Murat Göksel <hmgoksel@gmail.com>
2023-04-05 15:28:47 -0400

    Validate presence of Order attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database
 # with its default values. The data can then be loaded with the bin/rails
 # db:seed command (or created alongside the database with db:setup).
@@ -21,7 +22,8 @@ Broker.create!(
   mc: '123456',
   name: 'ShipCurve',
   phone: '1234567890',
-  usdot: '123456')
+  usdot: '123456'
+)
 Carrier.create!(
   address: '620 Alden Road, Suite 105, Markham, Ontario,L3R 9R7',
   country: 0,
@@ -29,13 +31,15 @@ Carrier.create!(
   mc: '123456',
   name: 'Little Guys Delivery Inc',
   phone: '9055139602',
-  usdot: '123456')
+  usdot: '123456'
+)
 Shipper.create!(
   address: '5105M Fisher Street Saint-Laurent, QC  H4T 1J8 Canada',
   country: 0,
   email: 'someone@naturestouchfrozenfoods.com',
   name: 'Natures Touch Frozen Foods',
-  phone: '9055139602')
+  phone: '9055139602'
+)
 Order.create!(
   carrier_id: Carrier.first.id,
   delivery_address: '5105M Fisher Street Saint-Laurent, QC  H4T 1J8 Canada',
@@ -44,11 +48,13 @@ Order.create!(
   orderer_type: 'Shipper',
   pickup_date: '2023-05-01',
   pickup_address: '975 Pacific Gate unit 18, Mississauga, ON L5T 1Y1',
-  status: 0)
+  status: 0
+)
 User.create!(
   email: 'ash@shipcurve.com',
   name: 'Ash Singh',
   org_id: Broker.first.id,
   org_type: 'Broker',
   password: 'password',
-  phone: '1234567890')
+  phone: '1234567890'
+)
