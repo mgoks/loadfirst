c279d523ce03067023feaac3069936152d99d219
Murat Göksel <hmgoksel@gmail.com>
2023-04-05 22:18:25 -0400

    Add unit tests for Order model
    Made the `carrier_id` column of `orders` table nullable since an Order
    will not always have a Carrier.

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,67 +10,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_401_200_109) do
-  create_table 'brokers', force: :cascade do |t|
-    t.string 'address'
-    t.integer 'country'
-    t.string 'email'
-    t.string 'mc'
-    t.string 'name'
-    t.string 'phone'
-    t.string 'usdot'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_021250) do
+  create_table "brokers", force: :cascade do |t|
+    t.string "address"
+    t.integer "country"
+    t.string "email"
+    t.string "mc"
+    t.string "name"
+    t.string "phone"
+    t.string "usdot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'carriers', force: :cascade do |t|
-    t.string 'address'
-    t.integer 'country'
-    t.string 'email'
-    t.string 'mc'
-    t.string 'name'
-    t.string 'phone'
-    t.string 'usdot'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "carriers", force: :cascade do |t|
+    t.string "address"
+    t.integer "country"
+    t.string "email"
+    t.string "mc"
+    t.string "name"
+    t.string "phone"
+    t.string "usdot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'orders', force: :cascade do |t|
-    t.integer 'carrier_id', null: false
-    t.string 'delivery_address'
-    t.integer 'equipment_type'
-    t.string 'orderer_type', null: false
-    t.integer 'orderer_id', null: false
-    t.date 'pickup_date'
-    t.string 'pickup_address'
-    t.integer 'status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['carrier_id'], name: 'index_orders_on_carrier_id'
-    t.index %w[orderer_type orderer_id], name: 'index_orders_on_orderer'
+  create_table "orders", force: :cascade do |t|
+    t.integer "carrier_id"
+    t.string "delivery_address"
+    t.integer "equipment_type"
+    t.string "orderer_type", null: false
+    t.integer "orderer_id", null: false
+    t.date "pickup_date"
+    t.string "pickup_address"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["carrier_id"], name: "index_orders_on_carrier_id"
+    t.index ["orderer_type", "orderer_id"], name: "index_orders_on_orderer"
   end
 
-  create_table 'shippers', force: :cascade do |t|
-    t.string 'address'
-    t.integer 'country'
-    t.string 'email'
-    t.string 'name'
-    t.string 'phone'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "shippers", force: :cascade do |t|
+    t.string "address"
+    t.integer "country"
+    t.string "email"
+    t.string "name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'name'
-    t.string 'org_type', null: false
-    t.integer 'org_id', null: false
-    t.string 'password_digest'
-    t.string 'phone'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[org_type org_id], name: 'index_users_on_org'
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "org_type", null: false
+    t.integer "org_id", null: false
+    t.string "password_digest"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["org_type", "org_id"], name: "index_users_on_org"
   end
 
-  add_foreign_key 'orders', 'carriers'
+  add_foreign_key "orders", "carriers"
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,7 +3,52 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :orders
+
+  test 'should not save order without delivery address' do
+    order = Order.new
+    assert_not order.save
+  end
+
+  test 'should not save order without equipment type' do
+    order = Order.new
+    assert_not order.save
+  end
+
+  test 'should not save order without orderer id' do
+    order = Order.new
+    assert_not order.save
+  end
+
+  test 'should not save order without orderer type' do
+    order = Order.new
+    assert_not order.save
+  end
+
+  test 'should not save order without pickup address' do
+    order = Order.new
+    assert_not order.save
+  end
+
+  test 'should not save order without pickup date' do
+    order = Order.new
+    assert_not order.save
+  end
+
+  test 'should not save order without status' do
+    order = Order.new
+    assert_not order.save
+  end
+
+  test 'order attributes must not be empty' do
+    order = Order.new
+    assert order.invalid?
+    assert order.errors[:delivery_address].any?
+    assert order.errors[:equipment_type].any?
+    assert order.errors[:orderer_id].any?
+    assert order.errors[:orderer_type].any?
+    assert order.errors[:pickup_address].any?
+    assert order.errors[:pickup_date].any?
+    assert order.errors[:status].any?
+  end
 end
